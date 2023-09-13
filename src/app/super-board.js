4dd88ca0c39d5f666f7b5f11ea144f236620e25f
Mars <549258+rmars@users.noreply.github.com>
2023-09-13 14:47:55 -0700

    Add a Reset Game / Play Again button

--- src/app/super-board.js
+++ src/app/super-board.js
@@ -1,10 +1,22 @@
 "use client";
-import { useMemo, useCallback, useReducer, createContext } from "react";
+import { useReducer, createContext } from "react";
 import Board from "./board";
 import calculateWinner from "./util";
 
 // https://legacy.reactjs.org/docs/hooks-faq.html#how-to-avoid-passing-callbacks-down
 const BoardDispatch = createContext(null);
+
+const newGameState = {
+  xIsNext: true,
+  currentPlayer: "X",
+  squares: Array(9).fill(Array(9).fill(null)),
+  lastPlay: Array(9).fill(null),
+  lastPlayedSquare: null,
+  boardToPlay: null,
+  boardResults: Array(9).fill(null),
+  winner: null,
+  winLine: null,
+};
 
 export default function SuperBoard() {
   const [state, dispatch] = useReducer(
@@ -80,6 +92,15 @@ export default function SuperBoard() {
             winner,
             winLine,
           };
+        case "RESET":
+          if (action.payload.mustConfirm) {
+            if (window.confirm("Leave this game and reset all game state?")) {
+              return newGameState;
+            }
+            // otherwise, do nothing
+            return state;
+          }
+          return newGameState;
       }
     },
     {
@@ -109,47 +130,57 @@ export default function SuperBoard() {
 
   return (
     <BoardDispatch.Provider value={dispatch}>
-      <>
+      <div className="center">
         <h1 className={state.winner !== null ? "shake" : ""}>
           Super Tic-Tac-Toe!
         </h1>
-        <div className="center">
-          {state.winner && (
-            <div className="status status-win shake">
-              {state.winner === "D" ? "DRAW!!!" : `WINNER: ${state.winner}`}
-            </div>
-          )}
-          {!state.winner && (
-            <div className="status">Current player: {state.currentPlayer}</div>
-          )}
-          {[1, 2, 3].map(i => (
-            <div key={i} className="board-row game">
-              {[1, 2, 3].map(j => {
-                const board = (i - 1) * 3 + (j - 1);
 
-                return (
-                  <Board
-                    key={`super-board-${board}`}
-                    dispatch={dispatch}
-                    squares={state.squares[board]}
-                    boardNumber={board}
-                    isPlayable={
-                      state.boardToPlay === null || state.boardToPlay === board
-                    }
-                    lastPlay={state.lastPlay[board]}
-                  />
-                );
-              })}
-            </div>
-          ))}
+        <button
+          className="button"
+          onClick={() =>
+            dispatch({
+              type: "RESET",
+              payload: { mustConfirm: !state.winner },
+            })
+          }>
+          {state.winner ? "Play Again" : "Reset Game"}
+        </button>
 
-          <a
-            target="_blank"
-            href="https://en.wikipedia.org/wiki/Ultimate_tic-tac-toe">
-            Rules (Wikipedia)
-          </a>
-        </div>
-      </>
+        {state.winner && (
+          <div className="status status-win shake">
+            {state.winner === "D" ? "DRAW!!!" : `WINNER: ${state.winner}`}
+          </div>
+        )}
+        {!state.winner && (
+          <div className="status">Current player: {state.currentPlayer}</div>
+        )}
+        {[1, 2, 3].map(i => (
+          <div key={i} className="board-row game">
+            {[1, 2, 3].map(j => {
+              const board = (i - 1) * 3 + (j - 1);
+
+              return (
+                <Board
+                  key={`super-board-${board}`}
+                  dispatch={dispatch}
+                  squares={state.squares[board]}
+                  boardNumber={board}
+                  isPlayable={
+                    state.boardToPlay === null || state.boardToPlay === board
+                  }
+                  lastPlay={state.lastPlay[board]}
+                />
+              );
+            })}
+          </div>
+        ))}
+
+        <a
+          target="_blank"
+          href="https://en.wikipedia.org/wiki/Ultimate_tic-tac-toe">
+          Rules (Wikipedia)
+        </a>
+      </div>
     </BoardDispatch.Provider>
   );
 }
